7e1360bbd0bd8b7495c937b4d009093092c312b9
zerist <15871792739@163.com>
2020-07-06 19:56:42 +0800

    bootstrap

--- weibo/resources/views/layouts/default.blade.php
+++ weibo/resources/views/layouts/default.blade.php
@@ -1,8 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>@yield('title', 'Weibo App')</title></head>
+    <title>@yield('title', 'Weibo App')</title>
+    <link rel="stylesheet" href="{{asset('/')}}/css/app.css">
+</head>
 <body>
-    @yield('content');
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">help</a> </li>
+                <li class="nav-item"><a class="nav-link" href="/about">about</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content');
+    </div>
 </body>
 </html>
--- weibo/resources/views/static_pages/home.blade.php
+++ weibo/resources/views/static_pages/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.default')
 @section('content')
-    <h1>Home</h1>
+    <div class="jumbotron">
+        <h1>hello laravel</h1>
+        <p class="lead">Here is <a href="#">Home!</a></p>
+        <p>Begin you weibo now.</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Register</a></p>
+    </div>
 @stop
