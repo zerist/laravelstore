e2a9248ec21620480ecf15314b92aaecda95c5b1
kanye <kanye.xu@i9i8.com>
2020-07-08 09:07:48 +0800

    paginate

--- weibo/resources/views/users/index.blade.php
+++ weibo/resources/views/users/index.blade.php
@@ -5,14 +5,13 @@
     <div class="offset-md-2 col-md-8">
         <h2 class="mb-4 text-center">All Users</h2>
         <div class="list-group list-group-flush">
-            @foreach($users as $user)
-                <div class="list-group-item">
-                    <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
-                    <a href="{{ route('users.show', $user) }}">
-                        {{ $user->name }}
-                    </a>
-                </div>
+            @foreach ($users as $user)
+                @include('users._user')
             @endforeach
+        </div>
+
+        <div class="mt-3">
+            {!! $users->render() !!}
         </div>
     </div>
 @stop
