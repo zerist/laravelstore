2b259aeea482af6f9061ea0af831bcb543025a97
kanye <kanye.xu@i9i8.com>
2020-07-09 14:45:25 +0800

    basic layout

---
+++ larabbs/resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer fixed-bottom bg-dark text-light p-3">
+    <div class="container">
+        <p class="float-left">
+            By <a class=" text-info" href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Kanye</a> Design <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a class=" text-info" href="mailto:1518079220@qq.com">Contact us</a></p>
+    </div>
+</footer>
