09d4d61a5f0e5be42cb1ccc8758333c53919c989
zerist <15871792739@163.com>
2020-07-28 22:54:52 +0800

    begin notify

--- larabbs/tests/test.php
+++ larabbs/tests/test.php
@@ -2,6 +2,18 @@
 require '../vendor/autoload.php';
 use \App\Handlers\SlugTranslateHandler;
 
-$o = new SlugTranslateHandler();
-$res = $o->translate('sadadad');
-echo $res;
+
+function alterArrayToQuery($list)
+{
+    $sql = "(";
+    foreach ($list as $item){
+        $sql .= strval($item);
+        if ($item !== end($list)) {
+            $sql .= ',';
+        }
+    }
+    $sql .= ")";
+    return $sql;
+}
+
+echo alterArrayToQuery([1,2,3,]);
