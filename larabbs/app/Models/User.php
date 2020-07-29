12016bb096cb231b111b6445008f7aa6d6c970c7
zerist <15871792739@163.com>
2020-07-29 11:51:32 +0800

    nitify reply

--- larabbs/app/Models/User.php
+++ larabbs/app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -47,5 +48,16 @@ class User extends Authenticatable implements MustVerifyEmail
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    //回复通知
+    public function notifyReplyToDB($notify)
+    {
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        $this->increment('notification_count', 1);
+        $this->notify($notify);
     }
 }
--- larabbs/app/Observers/ReplyObserver.php
+++ larabbs/app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
@@ -10,5 +11,8 @@ class ReplyObserver
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        //评论通知
+        $reply->topic->user->notifyReplyToDB(new TopicReplied($reply));
     }
 }
